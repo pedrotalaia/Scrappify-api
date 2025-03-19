5690015e170427bf90a88aa09d185f6ee2bf9bd3
Bruno Ferreira <brunoferreiraferreira17@gmail.com>
2025-03-19 21:54:17 +0000

    Adicionado controlo de sessões básico com login JWT

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { registerUser, deleteUser, updateUser } = require('../controllers/userController');
+const { registerUser, deleteUser, updateUser, loginUser } = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/register', registerUser);
 router.put('/:id', updateUser)
 router.delete('/:id', deleteUser);
+router.post('/login', loginUser);
 
 module.exports = router;
